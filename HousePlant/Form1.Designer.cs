99d59d1a0df362255a72a5e277fa514ed447296a
yu2519hn <1234568hH*>
2020-02-21 21:52:01 -0600

    Add project files.

---
+++ HousePlant/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace HousePlant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkSouthFacing = new System.Windows.Forms.CheckBox();
+            this.trkTemp = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSuggestion = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.InkHousePlantInfo = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.trkTemp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chkSouthFacing
+            // 
+            this.chkSouthFacing.AutoSize = true;
+            this.chkSouthFacing.Location = new System.Drawing.Point(37, 39);
+            this.chkSouthFacing.Name = "chkSouthFacing";
+            this.chkSouthFacing.Size = new System.Drawing.Size(202, 17);
+            this.chkSouthFacing.TabIndex = 0;
+            this.chkSouthFacing.Text = "Do you have a south-facing window?";
+            this.chkSouthFacing.UseVisualStyleBackColor = true;
+            this.chkSouthFacing.CheckedChanged += new System.EventHandler(this.HouseConditionsChanged);
+            // 
+            // trkTemp
+            // 
+            this.trkTemp.Location = new System.Drawing.Point(55, 118);
+            this.trkTemp.Maximum = 90;
+            this.trkTemp.Minimum = 50;
+            this.trkTemp.Name = "trkTemp";
+            this.trkTemp.Size = new System.Drawing.Size(218, 45);
+            this.trkTemp.TabIndex = 4;
+            this.trkTemp.Value = 50;
+            this.trkTemp.Scroll += new System.EventHandler(this.trkTemp_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "How warm is your home?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblSuggestion
+            // 
+            this.lblSuggestion.AutoSize = true;
+            this.lblSuggestion.Location = new System.Drawing.Point(165, 182);
+            this.lblSuggestion.Name = "lblSuggestion";
+            this.lblSuggestion.Size = new System.Drawing.Size(108, 13);
+            this.lblSuggestion.TabIndex = 10;
+            this.lblSuggestion.Text = "Plan Suggestion here";
+            this.lblSuggestion.Click += new System.EventHandler(this.lblSuggestion_Click);
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Location = new System.Drawing.Point(296, 130);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(29, 13);
+            this.lblTemp.TabIndex = 11;
+            this.lblTemp.Text = "50°F";
+            // 
+            // InkHousePlantInfo
+            // 
+            this.InkHousePlantInfo.AutoSize = true;
+            this.InkHousePlantInfo.Location = new System.Drawing.Point(270, 217);
+            this.InkHousePlantInfo.Name = "InkHousePlantInfo";
+            this.InkHousePlantInfo.Size = new System.Drawing.Size(100, 13);
+            this.InkHousePlantInfo.TabIndex = 12;
+            this.InkHousePlantInfo.TabStop = true;
+            this.InkHousePlantInfo.Text = "houseplant411.com";
+            this.InkHousePlantInfo.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.InkHousePlantInfo_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GreenYellow;
+            this.ClientSize = new System.Drawing.Size(382, 239);
+            this.Controls.Add(this.InkHousePlantInfo);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.lblSuggestion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trkTemp);
+            this.Controls.Add(this.chkSouthFacing);
+            this.Name = "Form1";
+            this.Text = "Houseplant Suggestions";
+            ((System.ComponentModel.ISupportInitialize)(this.trkTemp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkSouthFacing;
+        private System.Windows.Forms.TrackBar trkTemp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSuggestion;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.LinkLabel InkHousePlantInfo;
+    }
+}
+
---
+++ HousePlant/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HousePlant
+{
+    public partial class Form1 : Form
+    {
+        readonly int MinTemp = 50; //Global variables, available to all methods
+        readonly int MaxTemp = 90; //Convention is to use UppercaseCamelCase names
+        public Form1()
+        {
+            InitializeComponent();
+            this.trkTemp.Scroll += new System.EventHandler(this.HouseConditionsChanged);
+            this.trkTemp.Minimum = MinTemp;
+            this.trkTemp.Maximum = MaxTemp;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trkTemp_Scroll(object sender, EventArgs e)
+        {
+            //Use format string, the # symbol is replace by the number in tryTemp.Value
+            //Pressing Alt + 0176 on your number pad types a ° symbol
+            lblTemp.Text = trkTemp.Value.ToString("# ºF");
+        }
+
+        private void HouseConditionsChanged(object sender, EventArgs e)
+        {
+            int homeTemp = trkTemp.Value;
+            bool southFacingWindowAvailable = chkSouthFacing.Checked;
+            if (homeTemp == MinTemp)
+            {
+                MessageBox.Show(text: "Your home may be too cold for most houseplants", caption: "Information");
+            }
+            if (homeTemp == MaxTemp)
+            {
+                //Can you show a messege with caption "Information" and
+                //and text "Your home may be too warm for most plants"
+                //Use the named parameters for the text and caption parameters
+                MessageBox.Show(text: "Your home may be too hot for most houseplants", caption: "Information");
+            }
+            //Call our method, use return value
+            string suggestedPlant = GenerateSuggestion(homeTemp, southFacingWindowAvailable);
+            lblSuggestion.Text = suggestedPlant;
+        }
+        private string GenerateSuggestion(int temp, bool southFacing)
+        {
+            if (southFacing)
+            { 
+                if (temp > 65)
+                {
+                    return "Peace Lily"; //Warm with light
+                }
+                else
+                {
+                    return "Spider Plant"; //cool with light
+                }
+            }
+            else
+            {
+                if (temp > 65)
+                {
+                    return "Dragon Tree"; //Warm with low light
+                }
+                else
+                {
+                    return "Ivy"; //Cool with low light
+                }
+            }
+        }
+
+        private void lblSuggestion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InkHousePlantInfo_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (lblSuggestion.Text == "Plant suggestion here")
+            {
+                ShowWebPage(); //plantName is optional
+            }
+            else
+            {
+                ShowWebPage(lblSuggestion.Text);
+            }
+        }
+        private void ShowWebPage(string plantName = null) //Create new method
+        {
+            string url = "https://www.houseplant411.com/";
+            if (plantName != null)
+            {
+                //Link to a specific plant should be in the form "https://www.houseplant411.com/houseplant?hpg=ivy"
+                url = url + "houseplant?hpq=" + plantName;
+            }    
+            System.Diagnostics.Process.Start(url); //Launch web browser, navigate to URL given
+            
+        }
+    }
+}
+
